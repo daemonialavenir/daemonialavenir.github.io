2b538b411859bec935dd7027b359f56a3123a64d
sappocube <153672711+sappocube@users.noreply.github.com>
2024-01-14 20:11:50 +0100

    sraka

---
+++ load.js
@@ -0,0 +1,11 @@
+
+$(function footer() {
+    $("#foot").load("/end.html");
+});
+
+$(function navi() {
+    $("#navi").load("/navi.html");
+});
+$(function more() {
+    $("#more").load("/more.html");
+});
